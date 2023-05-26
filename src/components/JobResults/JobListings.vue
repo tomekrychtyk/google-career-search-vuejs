62e7b1a14fb51c968e03d8f0356ae1d1bcbd2edc
Tomek Rychtyk <tomek@rychtyk.pl>
2023-05-27 00:49:54 +0200

    Added pagination

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,8 +1,28 @@
 <template>
   <main class="bg-brand-gray-2 flex-auto p-8">
     <ol>
-      <job-listing v-for="job in jobs" :key="job.id" :job="job" />
+      <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
+    <div class="mx-auto mt-8">
+      <div class="flex flex-row flex-nowrap">
+        <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Previous</router-link
+          >
+
+          <router-link
+            v-if="nextPage"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Next</router-link
+          >
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -17,6 +37,30 @@ export default {
   data() {
     return {
       jobs: []
+    }
+  },
+  computed: {
+    previousPage() {
+      const previousPage = this.currentPage - 1
+      const firstPage = 1
+
+      return previousPage >= firstPage ? previousPage : undefined
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1
+      const lastPage = this.jobs.length / 10
+
+      return nextPage <= lastPage ? nextPage : undefined
+    },
+    currentPage() {
+      return Number.parseInt(this.$route.query.page)
+    },
+    displayedJobs() {
+      const pageNumber = this.currentPage
+      const firstJobIndex = (pageNumber - 1) * 10
+      const lastJobIndex = pageNumber * 10
+
+      return this.jobs.slice(firstJobIndex, lastJobIndex)
     }
   },
   async mounted() {
--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -7,23 +7,38 @@ import JobListings from "@/components/JobResults/JobListings.vue"
 vi.mock("axios")
 
 describe("JobListings", () => {
-  it("fetches jobs", () => {
-    axios.get.mockResolvedValue({ data: [] })
-    render(JobListings)
-    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs")
-  })
+  const createRoute = (queryParams) => {
+    return {
+      query: {
+        page: "1",
+        ...queryParams
+      }
+    }
+  }
 
-  it("creates a job listing for every job", async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
-
+  const renderJobListings = ($route) => {
     render(JobListings, {
       global: {
         stubs: {
           RouterLink: RouterLinkStub
+        },
+        mocks: {
+          $route
         }
       }
     })
+  }
+
+  it("fetches jobs", () => {
+    axios.get.mockResolvedValue({ data: [] })
+    renderJobListings(createRoute())
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs")
+  })
+
+  it("maximum of 10 jobs", async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+    renderJobListings(createRoute({ query: { page: "5" } }))
     const jobListings = await screen.findAllByRole("listitem")
-    expect(jobListings).toHaveLength(15)
+    expect(jobListings).toHaveLength(10)
   })
 })
