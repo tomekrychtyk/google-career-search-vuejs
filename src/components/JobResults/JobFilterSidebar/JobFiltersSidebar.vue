6d1d3dcb58f7a6fb8200947770225d5ec4c43f0f
Tomek Rychtyk <tomek@rychtyk.pl>
2023-05-31 23:06:14 +0200

    Added CollapsibleAccordion

--- src/components/JobResults/JobFilterSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFiltersSidebar.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="flex w-96 flex-col border-r border-solid border-brand-grey-1 bg-white p-4">
-    Job filters sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear filters" type="secondary" />
+        </div>
+      </div>
+
+      <collapsible-accordion />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue"
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue"
+
 export default {
-  name: "JobFiltersSidebar"
+  name: "JobFiltersSidebar",
+  components: {
+    ActionButton,
+    CollapsibleAccordion
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faSearch } from "@fortawesome/free-solid-svg-icons"
+import { faAngleDown, faAngleUp, faSearch } from "@fortawesome/free-solid-svg-icons"
 import { createPinia } from "pinia"
 
 import "@/index.css"
@@ -9,6 +9,9 @@ import router from "@/router"
 import App from "@/App.vue"
 
 library.add(faSearch)
+library.add(faAngleDown)
+library.add(faAngleUp)
+
 const pinia = createPinia()
 
 createApp(App).use(pinia).use(router).component("FontAwesomeIcon", FontAwesomeIcon).mount("#app")
