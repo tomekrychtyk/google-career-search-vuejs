37e15938032409d1f72dfba269a195c8f593d84e
Tomek Rychtyk <tomek@rychtyk.pl>
2023-05-31 23:41:33 +0200

    Added UT for CollapsibleAccordion slot

--- src/components/Shared/CollapsibleAccordion.vue
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="border-b border-solid border-brand-grey-2 py-5">
-    <div class="flex cursor-pointer flex-wrap items-center justify-between" @click="open">
+    <div
+      class="flex cursor-pointer flex-wrap items-center justify-between"
+      role="button"
+      @click="open"
+    >
       <h3 class="text-base font-semibold">{{ header }}</h3>
       <font-awesome-icon :icon="caretIcon" />
     </div>
---
+++ tests/unit/components/Shared/CollapsibleAccordion.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/vue"
+import userEvent from "@testing-library/user-event"
+
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue"
+
+describe("CollapsibleAccordion", () => {
+  it("renders child content", async () => {
+    render(CollapsibleAccordion, {
+      global: {
+        stubs: {
+          FontAwesomeIcon: true
+        }
+      },
+      props: {
+        header: "My Category"
+      },
+      slots: {
+        default: "<h3>My nested child</h3>"
+      }
+    })
+
+    expect(screen.queryByText("My nested child")).not.toBeInTheDocument()
+    const button = screen.getByRole("button", {
+      name: /my category/i
+    })
+    await userEvent.click(button)
+    expect(screen.queryByText("My nested child")).toBeInTheDocument()
+  })
+})
