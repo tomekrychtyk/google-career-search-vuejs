4fdf23bb9c29c3414d75e2232a7507c85f37798b
Tomek Rychtyk <tomek@rychtyk.pl>
2023-06-01 03:07:17 +0200

    Refactor

--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -17,19 +17,19 @@
       </div>
     </section>
     <spot-light class="flex flex-row justify-center pb-16">
-      <template #default="slotProps">
+      <template #default="{ img, title, description }">
         <router-link
           to="/jobs/results"
           class="mx-5 flex h-96 w-72 flex-col rounded-lg border bg-brand-grey-2"
         >
-          <img class="object-contain" :src="slotProps.spotlight.img" />
+          <img class="object-contain" :src="img" />
           <div class="mt-3 h-48 px-6 py-4">
             <h3 class="text-lg font-medium">
-              {{ slotProps.spotlight.title }}
+              {{ title }}
             </h3>
 
             <p class="mt-3 text-sm">
-              {{ slotProps.spotlight.description }}
+              {{ description }}
             </p>
           </div>
           <router-link to="/jobs/results" class="px-6 pb-4 text-sm text-brand-blue-1"
