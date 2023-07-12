8d56f4abdb03090f662b8299d8ca0297a72561ef
Tomek Rychtyk <tomek@rychtyk.pl>
2023-07-13 01:24:08 +0200

    More sidebar refactor

--- src/components/JobResults/JobFilterSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFiltersSidebar.vue
@@ -1,16 +1,7 @@
 <template>
   <div class="flex w-96 flex-col border-r border-solid border-brand-grey-1 bg-white p-4">
     <section class="pb-5">
-      <div class="flex flex-row justify-between">
-        <h3 class="my-4 text-base">What do you want to do?</h3>
-        <div class="flex items-center text-sm">
-          <action-button
-            text="Clear filters"
-            type="secondary"
-            @click="userStore.CLEAR_USER_JOB_FILTER_SELECTIONS"
-          />
-        </div>
-      </div>
+      <job-filter-sidebar-prompt />
 
       <collapsible-accordion header="Degrees">
         <job-filter-sidebar-degrees />
@@ -28,14 +19,9 @@
 </template>
 
 <script lang="ts" setup>
-import ActionButton from "@/components/Shared/ActionButton.vue"
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue"
-
 import JobFilterSidebarDegrees from "@/components/JobResults/JobFilterSidebar/JobFiltersSidebarDegrees.vue"
 import JobFilterSidebarJobTypes from "@/components/JobResults/JobFilterSidebar/JobFiltersSidebarJobTypes.vue"
 import JobFilterSidebarOrganizations from "@/components/JobResults/JobFilterSidebar/JobFiltersSidebarOrganizations.vue"
-
-import { useUserStore } from "@/stores/user"
-
-const userStore = useUserStore()
+import JobFilterSidebarPrompt from "@/components/JobResults/JobFilterSidebar/JobFiltersSidebarPrompt.vue"
 </script>
